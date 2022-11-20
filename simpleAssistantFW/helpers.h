4584fb6c722b3b631be490593015f0734668c6c2
jfm92 <jfloresm92@gmail.com>
2022-11-20 16:51:17 +0100

    Adding function to print weather info, including weather icon

--- simpleAssistantFW/helpers.h
+++ simpleAssistantFW/helpers.h
@@ -1,4 +1,6 @@
-String monthName[12]  {"January" , 
+//Things that can be use on several parts of the code and simplify the usage of it
+
+String monthName[12]  = {"January" , 
                         "February", 
                         "March", 
                         "April", 
@@ -10,3 +12,13 @@ String monthName[12]  {"January" ,
                         "October", 
                         "November", 
                         "December"};
+
+struct weatherStruct{
+    long lEpoch;
+    uint8_t weatherIcon;
+    float fTemperature;
+    uint8_t uiRelativeHumidity;
+    const char* pcWindDirection;
+    float fWindSpeed;
+    float fAtmPressure;
+};
--- simpleAssistantFW/simpleAssistantFW.ino
+++ simpleAssistantFW/simpleAssistantFW.ino
@@ -1,6 +1,11 @@
+#include "helpers.h"
+
 void setup()
 {
-
+    Serial.begin(115200);
+    initEPD();
+    loadingScreenEPD(true, "192.168.1.1");
+    //weatherStruct weatherData;
 }
 
 void loop()
